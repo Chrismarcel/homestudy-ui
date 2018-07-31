c55130d6b0b02470285fbf6b0de06770980308fc
Chrismarcel <cmarcelj@gmail.com>
2018-07-31 14:29:40 +0100

    Design html page for classroom

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,34 @@
 // Begin handling modal toggle events
-const headerBtn = document.querySelector('.header__btn');
-headerBtn.addEventListener('click', function () {
-    document.querySelector('.modal').classList.remove('modal--close');
-    document.querySelector('.modal').classList.add('modal--open');
-    document.getElementById('overlay').style.display = 'block';
-});
 
-document.querySelector('.modal__close').addEventListener('click', function () {
-    closeModal();
-});
+if (location.pathname === '/') {
+    document.querySelector('#begin').addEventListener('click', function (event) {
+        if (event.target.id === 'begin') {
+            document.querySelector('.modal').classList.remove('modal--close');
+            document.querySelector('.modal').classList.add('modal--open');
+            document.getElementById('overlay').style.display = 'block';
+        }
+    });
+    document.getElementById('profile-icon').style.display = 'none';
 
-document.querySelector('#overlay').addEventListener('click', function () {
-    closeModal();
-});
+    document.querySelector('.modal__close').addEventListener('click', function () {
+        closeModal();
+    });
 
-// Simulate user activation
-document.querySelector('#submit-email').addEventListener('click', function (event) {
-    event.preventDefault();
-    closeModal();
-    document.querySelector('.header__btn').childNodes[0].nodeValue = 'Classroom';
-    document.querySelectorAll('.content__header')[1].innerText = 'Why Use This Curriculum';
-    document.getElementById('profile-icon').style.display = 'inline-block';
-});
+    document.querySelector('#overlay').addEventListener('click', function () {
+        closeModal();
+    });
+
+    // Simulate user activation
+    document.querySelector('#submit-email').addEventListener('click', function (event) {
+        event.preventDefault();
+        closeModal();
+        document.querySelector('.header__btn').childNodes[0].nodeValue = 'Classroom';
+        document.querySelector('.header__btn').setAttribute('href', './classroom.html');
+        document.querySelector('.header__btn').removeAttribute('id');
+        document.querySelectorAll('.content__header')[1].innerText = 'Why Use This Curriculum';
+        document.getElementById('profile-icon').style.display = 'inline-block';
+    });
+}
 
 const closeModal = () => {
     document.querySelector('.modal').classList.remove('modal--open');
